ea760fbfd3f691321009feffc67cac00e27fa14f
cjswo4034 <cjswo4034@gmail.com>
2022-11-10 10:41:24 +0900

    refact(ds): ds 패키지 내의 인터페이스 제외하고 다 제거함

--- src/main/java/algorithm/sorting/stable/InsertionSorting.java
+++ src/main/java/algorithm/sorting/stable/InsertionSorting.java
@@ -5,5 +5,18 @@ import algorithm.sorting.Sorting;
 public class InsertionSorting implements Sorting {
     @Override
     public void sort(int[] arr) {
+        for (int i = 1, n = arr.length; i < n; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                change(arr, j - 1, j);
+                j--;
+            }
+        }
+    }
+
+    private void change(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
 }
--- src/main/java/algorithm/tree/Order.java
+++ src/main/java/algorithm/tree/Order.java
@@ -1,4 +1,33 @@
 package algorithm.tree;
 
+import java.util.List;
+
 public class Order {
+    public static List<Integer> inorder(List<Integer> bst, int i) {
+        return null;
+    }
+
+    public static List<Integer> preToIn(int[] pre) {
+        return null;
+    }
+
+    public static List<Integer> postToIn(int[] post) {
+        return null;
+    }
+
+    public static List<Integer> preorder(List<Integer> bst, int i) {
+        return null;
+    }
+
+    public static List<Integer> postToPre(int[] post) {
+        return null;
+    }
+
+    public static List<Integer> postorder(List<Integer> bst, int i) {
+        return null;
+    }
+
+    public static List<Integer> preToPost(int[] pre) {
+        return null;
+    }
 }
