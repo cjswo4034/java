1fc6fb5a577e269d92c84926bfef75100168eac9
cjswo4034 <cjswo4034@gmail.com>
2022-11-25 10:55:34 +0900

    refact(스타수열): 리팩토링 이전 풀이 추가

--- src/main/java/ps/pgmrs/lv3/스타수열.java
+++ src/main/java/ps/pgmrs/lv3/스타수열.java
@@ -1,5 +1,11 @@
 package ps.pgmrs.lv3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class 스타수열 {
     // 모범답안
     public int solution(int[] a) {
@@ -25,8 +31,9 @@ public class 스타수열 {
         return answer * 2;
     }
 
-    // 내풀이
+    // 내풀이 (after refactoring)
     public int solution2(int[] a) {
+        // 같은 max가 여러개 있을 수 있음
         int maxValue = count(a);
 
         int answer = getStarSequenceSize(maxValue, a);
@@ -70,4 +77,77 @@ public class 스타수열 {
         }
         return size;
     }
+
+    // before refactoring
+    public int solution3(int[] a) {
+        Count[] counts = IntStream.rangeClosed(0, getMax(a))
+                .mapToObj(idx -> new Count(idx, 0))
+                .toArray(Count[]::new);
+
+        count(a, counts);
+
+        Arrays.sort(counts, Comparator.comparingInt(Count::getCount));
+
+        int answer = getAnswer(a, counts);
+
+        return answer % 2 == 0 ? answer : answer - 1;
+    }
+
+    private int getAnswer(int[] a, Count[] counts) {
+        int result = 0;
+        int max = counts[0].count;
+        for (Count count : counts) {
+            if (count.count != max) break;
+            result = Math.max(result, getStarSequence(count.value, a).size());
+        }
+        return result;
+    }
+
+    private List<Integer> getStarSequence(int value, int[] a) {
+        List<Integer> starSequence = new ArrayList<>();
+
+        for (int e : a) {
+            int size = starSequence.size();
+            if (size % 2 == 1) {
+                int prev = starSequence.get(size - 1);
+                if ((e != value && prev == value) || (e == value && prev != value)) starSequence.add(e);
+            } else
+                starSequence.add(e);
+        }
+
+        return starSequence;
+    }
+
+    private void count(int[] a, Count[] counts) {
+        for (int i = 0, j; i < a.length; i++) {
+            j = i;
+            while (j < a.length && a[i] == a[j]) j++;
+            j--;
+
+            if (i != j) {
+                counts[a[i]].count += 2;
+                i = j;
+            } else
+                counts[a[i]].count++;
+        }
+    }
+
+    private int getMax(int[] a) {
+        return Arrays.stream(a).max().getAsInt();
+    }
+
+
+    class Count {
+        int value, count;
+
+        public Count(int value, int count) {
+            this.value = value;
+            this.count = count;
+        }
+
+        public int getCount() {
+            return -count;
+        }
+    }
 }
+
